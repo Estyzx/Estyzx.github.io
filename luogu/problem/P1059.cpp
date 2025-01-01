90ce94ebd5ff31cbbf4c8a05cc0fd08641420b3d
Estyzx <EstyZhong@outlook.com>
2025-01-01 16:55:43 +0800

    P1059

--- luogu/problem/P1059.cpp
+++ luogu/problem/P1059.cpp
@@ -1,22 +1,22 @@
-#include <bits/stdc++.h>  //万能头文件
+#include <bits/stdc++.h>  
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
-    set<int> st;  // set 容器
+    set<int> st;  
     for (int i = 0; i < n; i++)
     {
         int t;
         cin >> t;
         st.insert(t);
     }
-    vector<int> v(st.begin(), st.end());  // 将set容器转化为vector容器
-    sort(v.begin(), v.end());  //排序
+    vector<int> v(st.begin(), st.end());  
+    sort(v.begin(), v.end());  
     cout << v.size() << "\n";
     for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i] << " \n"[i == v.size() - 1];  //如果是最后一个元素，则输出换行符
+        cout << v[i] << " \n"[i == v.size() - 1];  
     }
     return 0;
 }
